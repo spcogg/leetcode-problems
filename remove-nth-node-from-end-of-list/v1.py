b5e1498b3691c386fb396a0d488f6368c574cc4b
spcogg <spcogg@gmail.com>
2019-05-07 23:45:41 +1000

    v1 linkedlists

--- remove-nth-node-from-end-of-list/v1.py
+++ remove-nth-node-from-end-of-list/v1.py
@@ -4,8 +4,14 @@
 #         self.val = x
 #         self.next = None
 
+# works, but is slow and memory intensive
+# speed: 18%
+# memory: 5.6%
+
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        # if head == [] or head ==None:
+        #     return []
         current = head
         maxcount = 1  # first loop to get length
         while(current.next != None):
@@ -13,10 +19,12 @@ class Solution:
             current = current.next
         current = head  # reset loop, do delete & re-point
         count = 1
-        while(current.next != None):
-            if count == maxcount:
-                # do checks on where it is in the cont
-                # and update the pointers accordingly
-            current = current.next        
-            count += 1
-                    
+        if maxcount == n:
+            return head.next
+        for count in range(1, maxcount):
+            # print(count)
+            if maxcount - (count) == n:  # difference between count and max = n from end
+                current.next = current.next.next
+                return head
+            current = current.next
+        return None
